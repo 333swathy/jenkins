5d41cfaed701bcef25e590e2114fcb8c4c219c74
GOKUL <GOKUL@DESKTOP-BHQF87V>
2024-11-15 11:52:26 +0530

    jenkin commit

---
+++ test_jenkins/src/main/java/helloworld/Hello.java
@@ -0,0 +1,17 @@
+package helloworld;
+
+public class Hello {
+	public void print()
+	{
+		System.out.println("Hello world");
+	}
+
+	public static void main(String[] args) 
+	{
+		Hello hl=new Hello();
+		hl.print();
+	
+
+	}
+
+}
